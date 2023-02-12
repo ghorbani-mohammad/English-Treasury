fdf0fe25eff228048b02223f2bcc495b1ab075df
Mohammad <mohammad.ghorbani@hotmail.com>
2023-02-12 15:53:29 +0330

    Add plural name to models

--- treasury/vocab/models.py
+++ treasury/vocab/models.py
@@ -9,7 +9,13 @@ class Vocabulary(BaseModel):
     short_description = models.TextField(null=True, blank=True)
     long_description = models.TextField(null=True, blank=True)
 
+    class Meta:
+        verbose_name_plural = "Vocabularies"
+
 
 class ProfileVocabulary(BaseModel):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     vocabulary = models.ForeignKey(Vocabulary, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "Profile Vocabularies"
