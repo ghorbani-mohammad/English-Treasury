54efab0fae13e1730144c304c57f10d000224b1f
Mohammad <mohammad.ghorbani@hotmail.com>
2023-02-12 14:23:01 +0330

    Add ProfileAdmin

--- treasury/user/admin.py
+++ treasury/user/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+from reusable.admins import ReadOnlyAdminDateFieldsMIXIN
+
+
+@admin.register(models.Profile)
+class ProfileAdmin(ReadOnlyAdminDateFieldsMIXIN, admin.ModelAdmin):
+    list_display = ("pk", "user", "created_at")
