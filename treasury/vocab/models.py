77e2835746d25f6a32fe28f0c4deaca61508fec0
Mohammad <mohammad.ghorbani@hotmail.com>
2023-02-12 14:19:55 +0330

    Minor change

--- treasury/vocab/models.py
+++ treasury/vocab/models.py
@@ -5,5 +5,5 @@ from reusable.models import BaseModel
 
 class Vocabulary(BaseModel):
     expression = models.CharField(max_length=20)
-    short_description = models.TextField(null=True)
-    long_description = models.TextField(null=True)
+    short_description = models.TextField(null=True, blank=True)
+    long_description = models.TextField(null=True, blank=True)
