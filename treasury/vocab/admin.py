525be5c01b2cd3da07d9cdc615b49fcb1f8fbe7b
Mohammad <mohammad.ghorbani@hotmail.com>
2023-02-12 12:40:29 +0330

    Add VocabularyAdmin

--- treasury/vocab/admin.py
+++ treasury/vocab/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Vocabulary)
+class VocabularyAdmin(admin.ModelAdmin):
+    list_display = ("pk", "expression", "created_at")
