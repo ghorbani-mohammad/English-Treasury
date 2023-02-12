b225361799c7326946cd41170da19dd02e3a1c7e
Mohammad <mohammad.ghorbani@hotmail.com>
2023-02-12 14:18:16 +0330

    Add Profile model Add long and short description to Vocabulary

--- treasury/user/models.py
+++ treasury/user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from reusable.models import BaseModel
+
+
+class Profile(BaseModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
--- treasury/vocab/models.py
+++ treasury/vocab/models.py
@@ -5,3 +5,5 @@ from reusable.models import BaseModel
 
 class Vocabulary(BaseModel):
     expression = models.CharField(max_length=20)
+    short_description = models.TextField(null=True)
+    long_description = models.TextField(null=True)
