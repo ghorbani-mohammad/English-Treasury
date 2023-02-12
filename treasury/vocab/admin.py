6e4ec9a0b026d5e07d8e80e331a76f44a64f7a84
Mohammad <mohammad.ghorbani@hotmail.com>
2023-02-12 14:39:19 +0330

    Add ProfileVocabularyAdmin

--- treasury/vocab/admin.py
+++ treasury/vocab/admin.py
@@ -7,3 +7,8 @@ from reusable.admins import ReadOnlyAdminDateFieldsMIXIN
 @admin.register(models.Vocabulary)
 class VocabularyAdmin(ReadOnlyAdminDateFieldsMIXIN, admin.ModelAdmin):
     list_display = ("pk", "expression", "created_at")
+
+
+@admin.register(models.ProfileVocabulary)
+class ProfileVocabularyAdmin(ReadOnlyAdminDateFieldsMIXIN, admin.ModelAdmin):
+    list_display = ("pk", "profile", "vocabulary", "created_at")
