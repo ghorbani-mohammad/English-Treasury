0cb46fb5ec604aded4371387b08ec2046a6b73d2
Mohammad <mohammad.ghorbani@hotmail.com>
2023-02-12 15:55:00 +0330

    Add unique=True to Vocabulary expression

--- treasury/vocab/models.py
+++ treasury/vocab/models.py
@@ -5,7 +5,7 @@ from reusable.models import BaseModel
 
 
 class Vocabulary(BaseModel):
-    expression = models.CharField(max_length=20)
+    expression = models.CharField(max_length=20, unique=True)
     short_description = models.TextField(null=True, blank=True)
     long_description = models.TextField(null=True, blank=True)
 
