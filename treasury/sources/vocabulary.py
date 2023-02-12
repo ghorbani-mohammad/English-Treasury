beb1ba01fb53cc3233307d629b57ca9d955a3a28
Mohammad <mohammad.ghorbani@hotmail.com>
2023-02-12 17:53:40 +0330

    Add get-definition function to VocabularyAPI

--- treasury/sources/vocabulary.py
+++ treasury/sources/vocabulary.py
@@ -5,12 +5,19 @@ from bs4 import BeautifulSoup as BS
 class VocabularyAPI:
     URL = "https://www.vocabulary.com"
 
-    def get_short_description(self, word):
+    def get_short_definition(self, word):
         resp = requests.get(f"{self.URL}/dictionary/{word}")
         doc = BS(resp.text, "html.parser")
         return doc.find("p", {"class": "short"}).text
 
-    def get_long_description(self, word):
+    def get_long_definition(self, word):
         resp = requests.get(f"{self.URL}/dictionary/{word}")
         doc = BS(resp.text, "html.parser")
         return doc.find("p", {"class": "long"}).text
+
+    def get_definition(self, word):
+        resp = requests.get(f"{self.URL}/dictionary/{word}")
+        doc = BS(resp.text, "html.parser")
+        short_definition = doc.find("p", {"class": "short"}).text
+        long_definition = doc.find("p", {"class": "long"}).text
+        return short_definition, long_definition
