bcb89a2692264e118044f53f848afbf6fb3e26be
Mohammad <mohammad.ghorbani@hotmail.com>
2023-02-12 14:37:18 +0330

    Add ProfileVocabulary model

--- treasury/vocab/models.py
+++ treasury/vocab/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from user.models import Profile
 from reusable.models import BaseModel
 
 
@@ -7,3 +8,8 @@ class Vocabulary(BaseModel):
     expression = models.CharField(max_length=20)
     short_description = models.TextField(null=True, blank=True)
     long_description = models.TextField(null=True, blank=True)
+
+
+class ProfileVocabulary(BaseModel):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    vocabulary = models.ForeignKey(Vocabulary, on_delete=models.CASCADE)
